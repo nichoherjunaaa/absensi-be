e3b7daee580561310fd2ff28a4d4f77aab40d059
nichoherjunaa <nichoherjunaa@gmail.com>
2025-02-23 22:28:26 +0700

    feat (mhs post get)

--- controller/mahasiswaController.js
+++ controller/mahasiswaController.js
@@ -1,18 +1,52 @@
 const Mahasiswa = require('../models/mahasiswaModel');
 const asyncHandler = require('express-async-handler');
+
 const getDataMahasiswa = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Success' });
+    const mahasiswa = await Mahasiswa.findAll();
+
+    if (!mahasiswa || mahasiswa.length === 0) {
+        return res.status(404).json({ message: 'Mahasiswa tidak ditemukan' });
+    }
+
+    res.status(200).json({
+        data: mahasiswa,
+        message: "Success",
+    });
 });
 
 const getDataById = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Success' });
+    const { username } = req.params;
+    const mahasiswa = await Mahasiswa.findByPk(username);
 
+    if (!mahasiswa) {
+        return res.status(404).json({ message: 'Mahasiswa tidak ditemukan' });
+    }
+
+    res.status(200).json({
+        data: mahasiswa,
+        message: "Success",
+    });
 });
 
 
 const addDataMahasiswa = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Success' });
+    const { nim, nama, program_studi, angkatan } = req.body;
 
+    if (!nim || !nama || !program_studi || !angkatan) {
+        return res.status(400).json({ message: 'Semua field harus diisi!' });
+    }
+
+    const existingMahasiswa = await Mahasiswa.findOne({ where: { nim } });
+    if (existingMahasiswa) {
+        return res.status(400).json({ message: 'NIM sudah terdaftar!' });
+    }
+
+    const mahasiswa = await Mahasiswa.create({ nim, nama, program_studi, angkatan });
+
+    res.status(201).json({
+        data: mahasiswa,
+        message: "Success",
+    });
 });
 
 const updateDataMahasiswa = asyncHandler(async (req, res) => {
